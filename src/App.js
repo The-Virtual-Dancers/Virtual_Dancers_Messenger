4a9a028d1b894fec02e372ee5ab17f3da1b66749
Ishan Manandhar <ishan02016@gmail.com>
2020-10-03 14:36:50 +0545

    added a name field where VD member can recognize who added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, {useState, useEffect} from 'react';
-import {Button, FormControl, InputLabel, Input} from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
-import Todo from './Todo'
-import db from './firebase'
+import Todo from './Todo';
+import db from './firebase';
 import firebase from 'firebase';
-
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  const [name, setName] = useState('');
 
   useEffect(() => {
-    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
-    })
-  }, [])
+    db.collection('todos')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data().todo,
+            name: doc.data().name,
+          }))
+        );
+      });
+  }, []);
 
   //This will fire off when we click the button
   const addTodo = (event) => {
@@ -22,29 +30,65 @@ function App() {
 
     db.collection('todos').add({
       todo: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    setInput('')
-  }
+      name: name,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput('');
+    setName('');
+  };
 
   return (
     <div className="App">
-      <h1>Virtual Dancers 👯‍♂️🕺💃 Secret Messenger</h1>
-      <form>
-      <FormControl>
-        <InputLabel>✅ Write a Message</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)}/>
-      </FormControl>
+      <h1>
+        Virtual Dancers{' '}
+        <span role="img" aria-label="emoji_dancers">
+          👯‍♂️🕺💃
+        </span>{' '}
+        Secret Messenger
+      </h1>
+      <form className="dancerForm">
+        <FormControl>
+          <InputLabel>
+            <span role="img" aria-label="emoji_ticked">
+              🧔🏾
+            </span>{' '}
+            Your name
+          </InputLabel>
+          <Input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel>
+            <span role="img" aria-label="emoji_ticked">
+              ✏️
+            </span>{' '}
+            Write a Message
+          </InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
 
-      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary" className="add_message_btn">Add Message</Button>
+        <Button
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+          className="add_message_btn"
+        >
+          Add Message
+        </Button>
       </form>
 
       <ul>
-        {todos.map(todo => (
-          <Todo todo={todo}/>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
         ))}
       </ul>
-
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,55 +1,102 @@
-import { ListItemAvatar, ListItemText } from '@material-ui/core'
-import React ,{useState}from 'react'
-import './Todo.css'
-import {ListItem, List, Modal, Button} from '@material-ui/core'
-import db from './firebase'
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import {makeStyles} from '@material-ui/core/styles'
+import { ListItemText } from '@material-ui/core';
+import React, { useState } from 'react';
+import './Todo.css';
+import { ListItem, List, Modal, Button } from '@material-ui/core';
+import db from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme)=> ({
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2,4,3)
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    left: '0',
+    top: '35%',
+    right: '0',
+    margin: 'auto',
+    width: '450px',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [input, setInput] = useState();
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+  const [name, setName] = useState('');
 
-    const updateTodo = () => {
-        db.collection('todos').doc(props.todo.id).set({
-            todo: input
-        }, {merge:true});
+  const updateTodo = () => {
+    db.collection('todos').doc(props.todo.id).set(
+      {
+        todo: input,
+        name: name,
+      },
+      { merge: true }
+    );
 
-        setOpen(false);
+    setOpen(false);
+  };
 
-    }
-
-    return (
-        <>
-        <Modal open = {open} onClose={e =>  setOpen(false)}>
-            <div className={classes.paper}>
-                <h1>Edit Your Message</h1>
-                <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)}></input>
-                <Button onClick={updateTodo}>Update Message</Button>
-            </div>
-        </Modal>
-        <List className="todo__list">
-            <ListItem>
-                <ListItemText primary={props.todo.todo} secondary="Virtual Dancers 👯‍♂️🕺💃" className="todo__list__item"/>
-                <Button variant="contained" onClick={e => setOpen(true)} className="todo__list__button">Edit</Button>
-                <Button variant="contained" onClick={event => db.collection('todos').doc(props.todo.id).delete()}>❌</Button>
-            </ListItem>
-            
-        </List>
-        </>
-    )
+  return (
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h1>Edit Your Message</h1>
+          <div className="editFields">
+            <label>Your name</label>
+            <input
+              placeholder={props.todo.name}
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            ></input>
+          </div>
+          <div className="editFields">
+            <label>Your message</label>
+            <input
+              placeholder={props.todo.todo}
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+            ></input>
+          </div>
+          <Button variant="contained" color="primary" onClick={updateTodo}>
+            Update Message
+          </Button>
+        </div>
+      </Modal>
+      <List className="todo__list">
+        <ListItem>
+          <ListItemText
+            primary={props.todo.todo}
+            secondary="Virtual Dancers 👯‍♂️🕺💃"
+            className="todo__list__item"
+          />
+          <ListItemText
+            primary={props.todo.name}
+            secondary="Dancer Name"
+            className="todo__list__item"
+          />
+          <Button
+            variant="contained"
+            onClick={(e) => setOpen(true)}
+            className="todo__list__button"
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            onClick={(event) =>
+              db.collection('todos').doc(props.todo.id).delete()
+            }
+          >
+            <span role="img" aria-label="emoji_delete">
+              ❌
+            </span>
+          </Button>
+        </ListItem>
+      </List>
+    </>
+  );
 }
 
-export default Todo
+export default Todo;
